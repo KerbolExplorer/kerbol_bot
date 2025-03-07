5ff31dbe1b8cc4a99feaa9bc0f65c13aee673bf7
Kerbol <50625120+KerbolExplorer@users.noreply.github.com>
2025-03-07 14:32:35 +0000

    The bot now changes activity every 3 hours

--- Solgaleo.py
+++ Solgaleo.py
@@ -2,17 +2,13 @@ import discord
 from discord.ext import commands
 import asyncio
 import random
-import sqlite3
 
 from bot_data import give_token
 token = give_token()
 
 bot = commands.Bot(command_prefix="S!", intents=discord.Intents.all())
 
-@bot.event
-async def on_ready():
-    print("Ready to dance!")
-
+async def change_activity():
     gameList = (
         "Persona 3 FES", "Persona 3 Reload", "Persona 4 Golden", "Persona 5 Royal", "Pokemon Sun", 
         "Pokemon Mystery Dungeon: Explorers of Sky", "Pokemon Super Mystery Dungeon", "Subnautica",
@@ -23,8 +19,16 @@ async def on_ready():
         "Sea of Stars", "Miitopia", "Slime Rancher", "Marvel Rivals", "Hollow Knight", "Hollow Knight: Zote boat", "Fortnite", "Amorous",
         "Ad Astra", "Metaphor: Refantazio", "Lethal Company"
         )
-    chosen_game = random.randint(0, (len(gameList) -1))
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(gameList[chosen_game]))
+    
+    while True:
+        chosen_game = random.randint(0, (len(gameList) -1))
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game(gameList[chosen_game]))
+        await asyncio.sleep(10800)
+
+@bot.event
+async def on_ready():
+    print("Ready to dance!")
+    bot.loop.create_task(change_activity())
 
     cogs_list_Test_Commands = ['Cogs.Test_Commands.Ping']
     cogs_list_dev = ['Cogs.Dev_Only.Run','Cogs.Dev_Only.Say']
