f6682a4ac89915125eb69d492abebcfa547e8009
Kerbol <50625120+KerbolExplorer@users.noreply.github.com>
2025-04-09 20:42:21 +0100

    updated About

--- Cogs/Misc/About.py
+++ Cogs/Misc/About.py
@@ -12,9 +12,7 @@ class About(commands.Cog):
             color=0xf1c40f, title="About", description="""Version 2.0. Bot developed by Kerbol (kobalt.621). 
             The bot is open source, link to the github: [super cool link]. 
             **Known Issues**:
-            -> Music may lag when someone adds a song to the queue.
-            -> Playlist may take a while to load up on the bot.
-            -> Cooldown to gain xp is currently global.
+            -> The music commands have been removed indefinetely due to issues with youtube
             """
             )
 
