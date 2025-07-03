e71a9c0a96e6ef1b7562215631423851febf14fd
Kerbol <50625120+KerbolExplorer@users.noreply.github.com>
2025-07-03 20:55:08 +0100

    Added update dev command

--- Cogs/Dev_Only/Dev_commands.py
+++ Cogs/Dev_Only/Dev_commands.py
@@ -64,8 +64,28 @@ class Dev_commands(commands.Cog):
                 return
         else:
             return
+    
+    #This command will update databases as required
+    @commands.command()
+    async def update(self, ctx):
+        try:
+            user_id = ctx.author.id
+            if self.verify_messenger(user_id) == True:
+                db = sqlite3.connect("db_exp.db")
+                cursor = db.cursor()
 
-        #S!add_airport KAVX small_airport "Catalina Airport" 33.404 -118.414878 1541 NA US Avalon US-CA no
+                sql = "UPDATE '606456281469026305' SET cooldownEnd = 0 WHERE userId = 442728041115025410"
+                cursor.execute(sql)
+                db.commit()
+                db.close()
+                await ctx.message.add_reaction("✅")
+            else:
+                return
+        except Exception as e:
+            print(e)
+
+
+    # S!add_airport KAVX small_airport "Catalina Airport" 33.404 -118.414878 1541 NA US Avalon US-CA no
     @commands.command()
     async def add_airport(self, ctx, ident, type, name, latitude, longitude, elevation:int, continent, country, location, region, scheduled_service):
         if self.verify_messenger(ctx.author.id) == True:
