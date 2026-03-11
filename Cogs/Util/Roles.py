dd9785a9f8d24eab327e169999afeaeffb3e71cf
Kerbol <50625120+KerbolExplorer@users.noreply.github.com>
2026-03-11 12:57:09 +0000

    Undid unique db connection

--- Cogs/Util/Roles.py
+++ Cogs/Util/Roles.py
@@ -6,13 +6,6 @@ import aiosqlite
 class Roles(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.db = None
-        self.cursor = None
-        self.bot.loop.create_task(self.start_db())
-    
-    async def start_db(self):
-        self.db = await aiosqlite.connect("db_exp.db")
-        self.cursor = await self.db.cursor()
 
     @app_commands.command(name="add-reaction-role", description="Add a reaction role to a message")
     @commands.has_guild_permissions(manage_messages=True)
@@ -35,19 +28,22 @@ class Roles(commands.Cog):
         if message.author.id != self.bot.user.id:
             await interaction.response.send_message("The message must be sent by me, use `/say` to make me send a message.", ephemeral=True)
             return
+        
+        db = await aiosqlite.connect("roles.db")
+        cursor = await db.cursor()
 
         sql = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{channel.guild.id}'"
-        await self.cursor.execute(sql)
-        result = await self.cursor.fetchall()
+        await cursor.execute(sql)
+        result = await cursor.fetchall()
         if not result:
             sql = f"CREATE TABLE '{channel.guild.id}' (id INTEGER, level INTEGER, msgId INTEGER, chnlId INTEGER, emoji TEXT, isTopRole INTEGER)"
-            await self.cursor.execute(sql)
+            await cursor.execute(sql)
         
         
         sql = f"INSERT INTO '{channel.guild.id}' (id, level, msgId, chnlId, emoji, isTopRole) VALUES (?, ?, ?, ?, ?, ?)"    # We add the guild data to the guilds table
-        await self.cursor.execute(sql, (role.id, 0, message_id, channel_id, emoji, 0))
-        await self.db.commit()
-        await self.db.close()
+        await cursor.execute(sql, (role.id, 0, message_id, channel_id, emoji, 0))
+        await db.commit()
+        await db.close()
         
         await message.add_reaction(emoji)
         await interaction.response.send_message("Done!", ephemeral=True)
@@ -59,17 +55,21 @@ class Roles(commands.Cog):
         elif payload.user_id == self.bot.user.id:
             return
         else:
+
+            db = await aiosqlite.connect("roles.db")
+            cursor = await db.cursor()
+
             # Check if the table exists, if it doesn't we just ignore it.
             sql = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{payload.guild_id}'"
-            await self.cursor.execute(sql)
-            result = await self.cursor.fetchall()
+            await cursor.execute(sql)
+            result = await cursor.fetchall()
             if not result:
-                await self.db.close()
+                await db.close()
                 return
 
             sql = f"SELECT * FROM '{payload.guild_id}' WHERE msgId = ? AND emoji = ?"
-            await self.cursor.execute(sql, (payload.message_id, str(payload.emoji)))
-            role_data = await self.cursor.fetchone()
+            await cursor.execute(sql, (payload.message_id, str(payload.emoji)))
+            role_data = await cursor.fetchone()
 
             member:discord.Member = payload.member
 
@@ -77,26 +77,30 @@ class Roles(commands.Cog):
             role = guild.get_role(role_data[0])
             
             await member.add_roles(role)
-            await self.db.close()
+            await db.close()
     
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
         if payload.user_id == self.bot.user.id:
             return
         else:
+
+            db = await aiosqlite.connect("roles.db")
+            cursor = await db.cursor()
+
             # Check if the table exists, if it doesn't we just ignore it.
             sql = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{payload.guild_id}'"
-            await self.cursor.execute(sql)
-            result = await self.cursor.fetchall()
+            await cursor.execute(sql)
+            result = await cursor.fetchall()
             if not result:
-                await self.db.close()
+                await db.close()
                 return
             
             emoji = str(payload.emoji)
 
             sql = f"SELECT * FROM '{payload.guild_id}' WHERE msgId = ? AND emoji = ?"
-            await self.cursor.execute(sql, (payload.message_id, emoji))
-            role_data = await self.cursor.fetchone()
+            await cursor.execute(sql, (payload.message_id, emoji))
+            role_data = await cursor.fetchone()
 
 
             guild:discord.Guild = self.bot.get_guild(payload.guild_id)
@@ -108,7 +112,7 @@ class Roles(commands.Cog):
             
             await member.remove_roles(role)
 
-            await self.db.close()
+            await db.close()
 """
     @app_commands.command(name="enable_custom_roles", description="Enables or disables custom roles on this server")
     @commands.has_guild_permissions(manage_messages=True)
@@ -246,8 +250,6 @@ class Roles(commands.Cog):
         await db.commit()
         await db.close()
         await ctx.send("Valid roles have been linked.")"""
-
-        
 
 async def setup(bot):
     await bot.add_cog(Roles(bot))
