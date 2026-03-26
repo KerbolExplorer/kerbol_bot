1f6789022c6f45bebd854078dd20719ab1af1caf
Kerbol <50625120+KerbolExplorer@users.noreply.github.com>
2026-03-26 14:10:28 +0000

    Added drop down for other forms

--- Cogs/Games/pokemon.py
+++ Cogs/Games/pokemon.py
@@ -7,17 +7,20 @@ import json
 import random
 
 class Pokemon(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot, file):
         self.bot = bot
+        self.file = file
     
     @dataclass
     class Pokemon_data:
         name:str
         number:int
+        base_species:str
         description:str
         height:str
         color:str
         weight:str
+        form:str
 
         abilites:dict
         types:tuple
@@ -90,128 +93,105 @@ class Pokemon(commands.Cog):
         # TODO: You can use serebii, dumbass
         # TODO: Add forms to a dropdown, would be pretty cool.
         pokemon = pokemon.lower()
-        with open("Bot_Databases/pokedex.json", 'r', encoding="utf-8") as file:
-            data:dict = json.load(file)
+        data:dict = self.file
 
-            data = data.get(pokemon)
+        data = data.get(pokemon)
 
-            if data == None:
-                return None
+        if data == None:
+            return None
 
-            # Image grabbing
-            api = "https://archives.bulbagarden.net/w/api.php"
+        # Image grabbing
+        api = "https://archives.bulbagarden.net/w/api.php"
             
-            titles = ""
-            titles = f"{data['num']:04d}"
+        titles = ""
+        titles = f"{data['num']:04d}"
             
-            #check for forms
-            form = data.get('forme', None)
+        #check for forms
+        form = data.get('forme', None)
 
-            home = "HOME"
+        home = "HOME"
 
-            if form:
-                if form == "Mega":
-                    titles = titles + "M"
-                elif form == "Mega-Z":
-                    # Game freak please put Z forms in home I beg you
-                    titles = f"{data['num']:04d}{data["baseSpecies"]}-Mega_Z"
-                    home = ""
-                elif form == "Gmax":
-                    titles = titles + "Gi"
+        if form:
+            if form == "Mega":
+                titles = titles + "M"
+            elif form == "Mega-Z":
+                # Game freak please put Z forms in home I beg you
+                titles = f"{data['num']:04d}{data["baseSpecies"]}-Mega_Z"
+                home = ""
+            elif form == "Gmax":
+                titles = titles + "Gi"
 
-            if shiny and form != "Mega-Z":
-                titles = f"{titles}_s"
+        if shiny and form != "Mega-Z":
+            titles = f"{titles}_s"
 
-            params = {
-                "action": "query",
-                "titles": f"File:{home}{titles}.png",
-                "prop": "imageinfo",
-                "iiprop": "url",
-                "format": "json"
-            }
+        params = {
+            "action": "query",
+            "titles": f"File:{home}{titles}.png",
+            "prop": "imageinfo",
+            "iiprop": "url",
+            "format": "json"
+        }
 
-            img_response = requests.get(api, params=params).json()
-            image_data = None
-            #{'batchcomplete': '', 'query': {'pages': {'-1': {'ns': 6, 'title': 'File:HOME0000.png', 'missing': '', 'imagerepository': ''}}}}
-            try:
-                if img_response is not None:
-                    pages = img_response.get("query").get("pages")
-                    image_data = list(pages.values())[0]["imageinfo"][0]["url"]
-            except Exception as e:
-                pass
+        img_response = requests.get(api, params=params).json()
+        image_data = None
+        #{'batchcomplete': '', 'query': {'pages': {'-1': {'ns': 6, 'title': 'File:HOME0000.png', 'missing': '', 'imagerepository': ''}}}}
+        try:
+            if img_response is not None:
+                pages = img_response.get("query").get("pages")
+                image_data = list(pages.values())[0]["imageinfo"][0]["url"]
+        except Exception as e:
+            pass
 
-            description_url = f"https://pokeapi.co/api/v2/pokemon-species/{pokemon}"
+        description_url = f"https://pokeapi.co/api/v2/pokemon-species/{pokemon}"
 
-            flavor_text = ""
-            response = requests.get(description_url)
-            if response.status_code == 404:
-                flavor_text = "Description not available."
-            else:
-                description_data = response.json()
-                # Grab the flavor text from this shitty api holy shit who made this
-                for description in description_data["flavor_text_entries"]:
-                    if description["language"]["name"] == "en":
-                        flavor_text = description["flavor_text"]
-                        break
+        flavor_text = ""
+        response = requests.get(description_url)
+        if response.status_code == 404:
+            flavor_text = "Description not available."
+        else:
+            description_data = response.json()
+            # Grab the flavor text from this shitty api holy shit who made this
+            for description in description_data["flavor_text_entries"]:
+                if description["language"]["name"] == "en":
+                    flavor_text = description["flavor_text"]
+                    break
 
-            to_return = self.Pokemon_data(name=data["name"],
-                                          number=data["num"],
-                                          description=flavor_text,
-                                          height=data["heightm"],
-                                          color=data["color"],
-                                          weight=data["weightkg"],
-                                          abilites=data["abilities"],
-                                          types=data["types"],
-                                          hp=data["baseStats"]["hp"],
-                                          attack=data["baseStats"]["atk"],
-                                          defense=data["baseStats"]["def"],
-                                          sp_attack=data["baseStats"]["spa"],
-                                          sp_defense=data["baseStats"]["spd"],
-                                          speed=data["baseStats"]["spe"],
-                                          image=image_data,
-                                          shiny_image=image_data,
-                                          is_shiny=shiny,
-                                          gender=data.get("genderRatio"),
-                                          evolution=None,
-                                          pre_evolutions=data.get("prevo"),
-                                          evo_type=data.get("evoType"),
-                                          evo_condition=data.get("evoCondition"),
-                                          egg_group=data["eggGroups"],
-                                          tier=data["tier"],
-                                          forms=data.get("otherFormes"))
+        to_return = self.Pokemon_data(name=data["name"],
+                                        number=data["num"],
+                                        base_species = data.get("baseSpecies", data.get("name")),
+                                        description=flavor_text,
+                                        height=data["heightm"],
+                                        color=data["color"],
+                                        weight=data["weightkg"],
+                                        form=data.get("form", None),
+                                        abilites=data["abilities"],
+                                        types=data["types"],
+                                        hp=data["baseStats"]["hp"],
+                                        attack=data["baseStats"]["atk"],
+                                        defense=data["baseStats"]["def"],
+                                        sp_attack=data["baseStats"]["spa"],
+                                        sp_defense=data["baseStats"]["spd"],
+                                        speed=data["baseStats"]["spe"],
+                                        image=image_data,
+                                        shiny_image=image_data,
+                                        is_shiny=shiny,
+                                        gender=data.get("genderRatio"),
+                                        evolution=None,
+                                        pre_evolutions=data.get("prevo"),
+                                        evo_type=data.get("evoType"),
+                                        evo_condition=data.get("evoCondition"),
+                                        egg_group=data["eggGroups"],
+                                        tier=data["tier"],
+                                        forms=data.get("otherFormes", None))
         return to_return
-
-    async def pokemon_autocomplete(self, interaction:discord.Interaction, current):
-        with open("Bot_Databases/pokedex.json", 'r', encoding="utf-8") as file:
-            data:dict = json.load(file)
-
-            tags = data.keys()
-
-            matches = [p for p in tags if current.lower() in p.lower()]
-
-            return [
-                app_commands.Choice(name=t, value=t)
-                for t in matches[:25]
-            ]
-
-
-    @app_commands.command(name="pokemon", description="Shows information about a pokemon.")
-    @app_commands.autocomplete(pokemon=pokemon_autocomplete)
-    @app_commands.describe(pokemon="Name of the pokemon to lookup")
-    async def pokemon_info(self, interaction:discord.Interaction, pokemon:str):
-        await interaction.response.defer()
+    
+    async def pokemon_embed(self, pokemon_data:Pokemon_data):
         shiny = random.randint(1, 1024)
 
         if shiny == 621:
             shiny = True
         else:
             shiny = False
-
-        pokemon_data = await self.get_pokemon(pokemon, shiny)
-
-        if pokemon_data == None:
-            await interaction.followup.send("Pokemon not found.")
-            return
         
         if shiny:
             embed = discord.Embed(
@@ -301,10 +281,82 @@ class Pokemon(commands.Cog):
 
         embed.set_footer(text="Data is from Showdown, Image is from Bulbagarden, description is from PokeAPI. Description is from the generation where the pokemon was introduced")
 
-        await interaction.followup.send(embed=embed)
+        return embed
+
+    async def pokemon_autocomplete(self, interaction:discord.Interaction, current):
+        data:dict = self.file
+
+        tags = data.keys()
+
+        matches = [p for p in tags if current.lower() in p.lower()]
+
+        return [
+            app_commands.Choice(name=t, value=t)
+                for t in matches[:25]
+            ]
+
+
+    @app_commands.command(name="pokemon", description="Shows information about a pokemon.")
+    @app_commands.autocomplete(pokemon=pokemon_autocomplete)
+    @app_commands.describe(pokemon="Name of the pokemon to lookup")
+    async def pokemon_info(self, interaction:discord.Interaction, pokemon:str):
+        await interaction.response.defer()
+        pokemon = pokemon.lower()
+
+        pokemon_data:Pokemon.Pokemon_data = await self.get_pokemon(pokemon=pokemon)
+
+        # Check if the pokemon exists
+        if not pokemon_data:
+            await interaction.followup.send("This pokemon does not exist or is not in my database.")
+            return
         
+        init_embed = await self.pokemon_embed(pokemon_data)
+        
+        forms = []
+        form_embeds = []
+        options = []
+        # Check if the pokemon is a variant of a base pokemon. If this is true we are dealing with a form.
+        if pokemon_data.base_species.lower() != pokemon or len(pokemon_data.forms) > 0:
+
+            
+            # All operations here should be done based on the original form. We also add the og form to the list also.
+            original_form = await self.get_pokemon(pokemon_data.base_species.lower())
+            forms.insert(0, original_form.name)
+            form_embeds.insert(0, await self.pokemon_embed(original_form))
+            options.append(discord.SelectOption(label=original_form.name, description="Shows information for this form", emoji="<:pokeball:1486725525052457050>"))
+
+            for form in original_form.forms:
+                forms.insert(0, form)
+                form:str = form.replace("-", "")
+                form = form.lower()
+                form_data = await self.get_pokemon(form)
+                form_embeds.insert(0, await self.pokemon_embed(form_data))
+
+                options.append(discord.SelectOption(label=form_data.name, description="Shows information for this form", emoji="<:megaevolution:1486725244537536662>"))
+
+            # If we have forms ready up the embeds and view.
+            class PokemonView(discord.ui.View):
+                def __init__(self, embeds):
+                    super().__init__()
+                    self.embeds = embeds
+            
+                @discord.ui.select(
+                    placeholder="Select a form",
+                    min_values=1,
+                    max_values=1,
+                    options = options
+                )
+        
+                async def select_callback(self, interaction:discord.Interaction, select:discord.ui.Select):
+                    index = forms.index(select.values[0])
+                    await interaction.response.edit_message(embed=self.embeds[index], view=self)
+
+            await interaction.followup.send(view=PokemonView(form_embeds), embed=init_embed)
+        else:
+            await interaction.followup.send(embed=init_embed)
 
 
 
 async def setup(bot):
-    await bot.add_cog(Pokemon(bot))
+    file = json.load(open("Bot_Databases/pokedex.json", 'r', encoding="utf-8"))
+    await bot.add_cog(Pokemon(bot, file))
