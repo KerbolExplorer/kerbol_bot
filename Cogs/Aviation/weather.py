e0a8d1fea3952db11c28c30a378b50ebaae659be
Kerbol <50625120+KerbolExplorer@users.noreply.github.com>
2026-09-15 09:56:52 +0100

    zulu command now works again

--- Cogs/Aviation/weather.py
+++ Cogs/Aviation/weather.py
@@ -266,7 +266,7 @@ class Weather(commands.Cog):
     
     @app_commands.command(name="zulu_time", description="Returns the current zulu time")
     async def zulu_time(self, interaction:discord.Interaction):
-        current_time = get_current_zulu()
+        current_time = get_current_zulu(False)
         current_time = str(current_time) + "Z"
         await interaction.response.send_message(f"The current zulu time is: `{current_time}`")
     
